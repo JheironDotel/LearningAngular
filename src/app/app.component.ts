9b2d391d592a37386a773b2d4cd6a73a9a6ccda9
Jheiron Dotel <jheirondotel@gmail.com>
2019-05-30 15:57:04 -0400

    Interpolacion
    Interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "CodeSandbox Test 01";
+  nombre = "Jheiron Dotel";
+  edad = 29;
+  email = "jdotel@yea.com";
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+
+  esActivo() {
+    if (this.activo) {
+      return "Trabajador activo";
+    } else {
+      return "Trabajador inactivo";
+    }
+  }
+
+  ultimo3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++) {
+      suma += this.sueldos[x];
+    }
+
+    return suma;
+  }
 }
